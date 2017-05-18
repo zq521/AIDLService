a89f98237a52c112c8ee579a1ba5303da094e25e
zhaoqiang <727319870@qq.com>
2017-05-18 14:21:10 +0800

    ...

--- app/src/main/java/com/example/zhaoqiang/service/MainActivity.java
+++ app/src/main/java/com/example/zhaoqiang/service/MainActivity.java
@@ -3,15 +3,19 @@ package com.example.zhaoqiang.service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+/**
+ * 启动服务，停止服务，绑定服务，解除服务
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
     private Intent myServiceIntent;
-    private MyService.MyBinder bind;
+    private MyService.MyBinder binder;
 
 
     @Override
@@ -19,45 +23,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myServiceIntent = new Intent(this, MyService.class);
-        findViewById(R.id.btnstartService).setOnClickListener(this);
-        findViewById(R.id.btnstoptService).setOnClickListener(this);
-        findViewById(R.id.btnsetServiceCountTo100).setOnClickListener(this);
-        findViewById(R.id.btnBindService).setOnClickListener(this);
-        findViewById(R.id.btnunBindService).setOnClickListener(this);
-        findViewById(R.id.btnSetCountTo100).setOnClickListener(this);
-        findViewById(R.id.get_count_to_100).setOnClickListener(this);
+        findViewById(R.id.btn_start_service).setOnClickListener(this);
+        findViewById(R.id.btn_stop_service).setOnClickListener(this);
+        findViewById(R.id.btn_set_service_count_To_100).setOnClickListener(this);
+        findViewById(R.id.btn_bind_service).setOnClickListener(this);
+        findViewById(R.id.btn_unBind_service).setOnClickListener(this);
+        findViewById(R.id.btn_set_bind_service_count_To_100).setOnClickListener(this);
+        findViewById(R.id.btn_get_bind_service_count_to_100).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnstartService:
+            case R.id.btn_start_service:
                 startService(myServiceIntent);
 
                 break;
-            case R.id.btnstoptService:
+            case R.id.btn_stop_service:
                 stopService(myServiceIntent);
                 break;
-            case R.id.btnsetServiceCountTo100:
+            case R.id.btn_set_service_count_To_100:
                 Intent intent = new Intent(this, MyService.class);
                 intent.putExtra("count", 100);
                 startService(intent);
                 break;
-            case R.id.btnBindService:
+            case R.id.btn_bind_service:
                 bindService(myServiceIntent, this, BIND_AUTO_CREATE);
                 break;
-            case R.id.btnunBindService:
-                bind=null;
+            case R.id.btn_unBind_service:
+                binder = null;
                 unbindService(this);
                 break;
-            case R.id.btnSetCountTo100:
-                if (bind!=null){
-                    bind.setCount(100);
+            case R.id.btn_set_bind_service_count_To_100:
+                if (binder != null) {
+                    binder.setCount(100);
                 }
                 break;
-            case R.id.get_count_to_100:
-                if (bind!=null) {
-                    Toast.makeText(this,""+bind.getConut(), Toast.LENGTH_SHORT).show();
+            case R.id.btn_get_bind_service_count_to_100:
+                if (binder != null) {
+                    Toast.makeText(this, "" + binder.getConut(), Toast.LENGTH_SHORT).show();
                 }
                 break;
 
@@ -67,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-        bind= (MyService.MyBinder) service;
+        binder = (MyService.MyBinder) service;
 
     }
 
